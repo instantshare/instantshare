2435ef166b504bb57715b3bdd411a1631f5a580d
dtext <textores.danny@gmail.com>
2015-10-27 15:52:48 +0100

    Check for platform, build file path, then call take_screenshot() from the platform-specific module

--- src/instantshare.py
+++ src/instantshare.py
@@ -1,1 +1,18 @@
-__author__ = 'dt'
+from tools.platform import Platform
+import screenshot.win
+import screenshot.linux
+from time import strftime
+from tempfile import gettempdir
+
+
+tmpdir = gettempdir()
+if Platform.this == Platform.LINUX:
+    take_screenshot = screenshot.linux.take_screenshot
+    tmpdir += '/'
+elif Platform.this == Platform.WINDOWS:
+    take_screenshot = screenshot.win.take_screenshot
+    tmpdir += '\\'
+
+file = tmpdir + "instantscreen_{}.png".format(strftime("%Y-%m-%d_%H-%I-%S"))
+take_screenshot(file)
+
