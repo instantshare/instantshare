7a6a6fc89d39c76fa7a3e6bbaf8f9e398ad7d680
dtext <textores.danny@gmail.com>
2015-10-28 17:47:26 +0100

    Create skeleton for dynamically loading Cloud Storage Providers

--- src/instantshare.py
+++ src/instantshare.py
@@ -1,7 +1,9 @@
 from tools.platform import Platform
 from time import strftime
 from tools.config import CONFIG
+from storage import *
 
+# take screenshot
 if Platform.this == Platform.LINUX:
     import screenshot.linux
     take_screenshot = screenshot.linux.take_screenshot
@@ -12,3 +14,12 @@ elif Platform.this == Platform.WINDOWS:
 file = CONFIG.get("General", "tmpdir") + "instantscreen_{}.png".format(strftime("%Y-%m-%d_%H-%I-%S"))
 take_screenshot(file)
 
+# initialize storage
+storage_providers = {
+#    "dropbox": Dropbox()
+#    "google-drive": Drive()
+     "test": test.Test()
+}
+sp = storage_providers[CONFIG.get("General", "storage")]
+sp.initialize()
+sp.upload(file)
