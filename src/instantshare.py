0ed0be0f810d528af24bd1c63d5e4f4533fcd053
florianluediger <florian.luediger@googlemail.com>
2015-11-11 22:29:06 +0100

    Add argument parser to accept command line inputs.

--- src/instantshare.py
+++ src/instantshare.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
+
 from time import strftime
 from tools.config import CONFIG
 from storage import *
 from tools.screenshot import Screen
 import webbrowser
+import argparse
+
+#parse arguments
+parser = argparse.ArgumentParser(description="Take a screenshot and upload it.")
+#dest defines the name of the key in the dictionary. The value will be true or false (action="store_true")
+parser.add_argument("-w", "--whole", dest="whole", action="store_true", help="Takes a screenshot of the whole screen.")
+parser.add_argument("-c", "--crop", dest="crop", action="store_true", help="Takes a screenshot and lets you crop it.")
+
+args = vars(parser.parse_args())
 
 # take screenshot
 s = Screen()
 file = CONFIG.get("General", "tmpdir") + "instantscreen_{}.png".format(strftime("%Y-%m-%d_%H-%I-%S"))
-s.take_screenshot_crop(file)
+
+if args.get("whole"):
+    s.take_screenshot_whole(file)
+else:
+    s.take_screenshot_crop(file)
 
 # initialize storage
 storage_providers = {
