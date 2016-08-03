8d4a92972cbad4201d24a266956405e6897c4c7e
dtext <textores.danny@gmail.com>
2016-08-03 22:57:31 +0200

    Simplify platform.py

--- src/tools/platform.py
+++ src/tools/platform.py
@@ -1,17 +1,8 @@
 from abc import ABCMeta, abstractmethod
-from enum import Enum
 from sys import platform as _platform
 
 
 class Platform(metaclass=ABCMeta):
-    LINUX, WINDOWS, OSX = range(3)
-
-    if _platform == "linux":
-        this = LINUX
-    elif _platform == "win32":
-        this = WINDOWS
-    elif _platform == "darwin":
-        this = OSX
 
     @abstractmethod
     def init_linux(self):
@@ -26,11 +17,12 @@ class Platform(metaclass=ABCMeta):
         pass
 
     def __init__(self):
-        if Platform.this == Platform.LINUX:
-            self.init_linux()
-        elif Platform.this == Platform.WINDOWS:
-            self.init_windows()
-        elif Platform.this == Platform.OSX:
-            self.init_osx()
-        else:
+        init_functions = {
+            "linux": self.init_linux,
+            "win32": self.init_windows,
+            "darwin": self.init_osx
+        }
+        if _platform not in init_functions.keys():
             raise OSError("Platform not supported!")
+        else:
+            init_functions[_platform]()
