e6507d2fce79e1ff3068756e57a5ed2728c0df86
Dennis Effing <dennis.effing@hotmail.com>
2016-08-23 22:28:41 +0200

    Update dropbox API to v2 and use new OAuth tool for authorization. Closes #9.

--- src/storage/dropbox.py
+++ src/storage/dropbox.py
@@ -1,44 +1,57 @@
-from tools.config import CONFIG
-import dropbox
-import ntpath
-import webbrowser
 import logging
+import ntpath
+
+import dropbox
+from dropbox.files import WriteMode
+
+from tools.config import CONFIG
+from tools.oauthtool import implicit_flow
 
 _name = "dropbox"
 
 
-def upload(file: str) -> str:
-    # TODO: Add routine for safely stored app_key and app_secret
+def _authorize():
+    authorization_endpoint = "https://www.dropbox.com/oauth2/authorize"
     app_key = CONFIG.get(_name, "app_key")
     app_secret = CONFIG.get(_name, "app_secret")
 
+    # Start OAuth2 implicit flow
+    auth_response = implicit_flow(authorization_endpoint, app_key)
+
+    # Check if authorization was successful
+    if auth_response["error"] is not None:
+        logging.error("Authentication failed. Error message: {0}".format(auth_response["error_description"]))
+        return False
+
+    # FIXME: Don't save access token in unencrypted config file
+    CONFIG.set(_name, "access_token", auth_response["access_token"])
+    CONFIG.set(_name, "account_id", auth_response["account_id"])
+    CONFIG.write()
+
+    return True
+
+
+def upload(file: str) -> str:
     # TODO: Use another way to find out if the access token is valid
     if CONFIG.get(_name, "access_token") == "0":
-        flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
-        authorize_url = flow.start()
-
-        webbrowser.open_new_tab(authorize_url)
-        code = input("Enter the authorization code here: ").strip()
-
-        access_token, user_id = flow.finish(code)
-
-        CONFIG.set(_name, "access_token", access_token)
-        CONFIG.set(_name, "user_id", user_id)
-        CONFIG.write()
+        _authorize()
 
     access_token = CONFIG.get(_name, "access_token")
-    user_id = CONFIG.get(_name, "user_id")
+    account_id = CONFIG.get(_name, "user_id")
 
-    f = open(file, 'rb')
-    # According to DropboxClient, this documentation is deprecated:
-    # https://www.dropbox.com/developers-v1/core/start/python
-    dropbox_client = dropbox.client.DropboxClient(access_token)
-    dropbox_filepath = CONFIG.get(CONFIG.general, "screenshot_dir") + "/" + ntpath.basename(file)
+    dropbox_client = dropbox.Dropbox(access_token)
+    dropbox_filepath = "/" + CONFIG.get(CONFIG.general, "screenshot_dir") + "/" + ntpath.basename(file)
+    file_object = open(file, 'rb')
 
     try:
-        response = dropbox_client.put_file(dropbox_filepath, f)
-    except dropbox.rest.ErrorResponse as e:
-        logging.error(e.error_msg)
+        file_metadata = dropbox_client.files_upload(file_object, dropbox_filepath, mode=WriteMode("overwrite", None),
+                                                    client_modified=None, mute=False)
+    except dropbox.exceptions.ApiError as e:
+        logging.error("Upload failed. Error message: {0}".format(e.error_msg))
 
-    url = dropbox_client.media(dropbox_filepath)["url"]
+    try:
+        url = dropbox_client.sharing_create_shared_link_with_settings(dropbox_filepath, None).url
+    except dropbox.exceptions.ApiError as e:
+        logging.error("Could not create shared link. Error message: {0}".format(e.error_msg))
+
     return url
