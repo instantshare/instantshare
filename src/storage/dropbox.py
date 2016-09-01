bc8b344ecf01394a11ca7b7231b5d45feeeecb3a
Dennis Effing <dennis.effing@hotmail.com>
2016-09-01 21:40:09 +0200

    Fix authentication error check. Minor fixes.

--- src/storage/dropbox.py
+++ src/storage/dropbox.py
@@ -14,10 +14,11 @@ def upload(file: str) -> str:
     # TODO: Use another way to find out if the access token is valid
     # FIXME: Check for authorization failure
     if CONFIG.get(_name, "access_token") == "0":
-        __authorize()
+        authorization_successful = _authorize()
+        if not authorization_successful:
+            return None  # unable to upload without successful authorization
 
     access_token = CONFIG.get(_name, "access_token")
-    account_id = CONFIG.get(_name, "user_id")
 
     dropbox_client = dropbox.Dropbox(access_token)
     dropbox_filepath = "/" + CONFIG.get(CONFIG.general, "screenshot_dir") + "/" + ntpath.basename(file)
@@ -28,25 +29,26 @@ def upload(file: str) -> str:
                                                     client_modified=None, mute=False)
     except dropbox.exceptions.ApiError as e:
         logging.error("Upload failed. Error message: {0}".format(e.error_msg))
+        return None
 
     try:
         url = dropbox_client.sharing_create_shared_link_with_settings(dropbox_filepath, None).url
     except dropbox.exceptions.ApiError as e:
         logging.error("Could not create shared link. Error message: {0}".format(e.error_msg))
+        return None
 
-    return __validate_url(url)
+    return _change_url_suffix(url)
 
 
-def __authorize():
+def _authorize():
     authorization_endpoint = "https://www.dropbox.com/oauth2/authorize"
     app_key = CONFIG.get(_name, "app_key")
-    app_secret = CONFIG.get(_name, "app_secret")
 
     # Start OAuth2 implicit flow
     auth_response = implicit_flow(authorization_endpoint, app_key)
 
     # Check if authorization was successful
-    if auth_response["error"] is not None:
+    if "error" in auth_response and auth_response["error"] is not None:
         logging.error("Authentication failed. Error message: {0}".format(auth_response["error_description"]))
         return False
 
@@ -58,7 +60,8 @@ def __authorize():
     return True
 
 
-def __validate_url(url):
+def _change_url_suffix(url):
+    # Modifies URL that it directly refers to the uploaded file.
     suffix = "dl=0"
     raw_suffix = "raw=1"
 
