29834a5e9ed180eae73b025033de8be515979360
dtext <textores.danny@gmail.com>
2016-09-07 21:24:27 +0200

    Port screenshot functionality from instantshare.py over to screen command

--- src/cli/screen.py
+++ src/cli/screen.py
@@ -1,7 +1,50 @@
 """
-Not implemented yet.
+Take a cropped screenshot and share it's public URL.
+
+Usage: instantshare.py screen [options]
+
+Options:
+  -w, --whole           Take a screenshot of the whole screen instead
+  --tool=<tool>         Overwrite the screenshot_tool config parameter
+  --storage=<provider>  Overwrite the storage config parameter
 """
+from docopt import docopt
+from tools.config import CONFIG
+from importlib import import_module
+from tempfile import gettempdir
+from time import strftime
 
 
 def main(argv):
-    print(__doc__)
+    args = docopt(__doc__, argv=argv)
+
+    # import modules dynamically
+    scrtool = args["--tool"] if args["--tool"] else CONFIG.get(CONFIG.general, "screenshot_tool")
+    storage = args["--storage"] if args["--storage"] else CONFIG.get(CONFIG.general, "storage")
+    scrtool = import_module("screenshot." + scrtool)
+    storage = import_module("storage." + storage)
+
+    # build filename
+    file = "{}/instantscreen_{}.png".format(gettempdir(), strftime("%Y-%m-%d_%H-%I-%S"))
+
+    # take screenshot
+    if args["--whole"]:
+        scrtool.take_screenshot_whole(file)
+    else:
+        scrtool.take_screenshot_crop(file)
+
+    # upload to storage
+    url = storage.upload(file)
+
+    # execute user defined action
+    if CONFIG.getboolean(CONFIG.general, "cb_autocopy"):
+        import tools.clipboard as c
+        c.Clipboard().set(url)
+    else:
+        import webbrowser as w
+        w.open_new_tab(url)
+
+    # notify user if set
+    if CONFIG.getboolean(CONFIG.general, "notification_sound"):
+        import tools.audio as a
+        a.play_notification()
