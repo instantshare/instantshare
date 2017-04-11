021e72e93f95a160fb2a1becc702fad7d64df39f
dtext <textores.danny@gmail.com>
2017-04-11 22:08:40 +0200

    Add the capability to instantly share generic files

--- src/cli/file.py
+++ src/cli/file.py
@@ -1,7 +1,24 @@
 """
-Not implemented yet.
+Take a cropped screenshot and share its public URL.
+
+Usage: instantshare file [options] <filename>
+
+Options:
+  -h, --help            Print this information
+  --storage=<provider>  Overwrite the storage config parameter
 """
+from docopt import docopt
+
+import storage
 
 
 def main(argv):
-    print(__doc__)
+    args = docopt(__doc__, argv=argv)
+
+    hoster = args["--storage"]
+    file = args["<filename>"]
+
+    if hoster:
+        storage.upload_to(hoster, file)
+    else:
+        storage.upload_generic_file(file)
