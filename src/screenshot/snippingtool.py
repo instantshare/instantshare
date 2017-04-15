d2e2b69455e94e72922dcc2c1d2814c8b4abaf63
Dennis Effing <dennis.effing@hotmail.com>
2017-04-15 22:56:40 +0200

    Add an alternative screenshot tool for Windows that uses Snipping Tool for capturing the screen.

---
+++ src/screenshot/snippingtool.py
@@ -0,0 +1,16 @@
+from subprocess import call
+from PIL import ImageGrab
+from libraries.pyrobot import Robot
+
+_r = Robot()
+
+
+def take_screenshot_whole(path):
+    image = _r.take_screenshot()
+    image.save(path, "PNG")
+
+
+def take_screenshot_crop(path):
+    call(["snippingtool", "/clip"])
+    image = ImageGrab.grabclipboard()
+    image.save(path, "png")
