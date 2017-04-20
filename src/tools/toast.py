db470300e796e4366d80c487fb921efbcb8eaf0a
dtext <textores.danny@gmail.com>
2017-04-20 18:38:44 +0200

    Add our icon as default icon for toast notifications

--- src/tools/toast.py
+++ src/tools/toast.py
@@ -1,3 +1,4 @@
+from tools import dirs
 from tools.toolbox import Platform
 
 
@@ -7,7 +8,7 @@ class Toast(Platform):
         super().__init__()
 
     def init_linux(self):
-        def show(title, message, icon=None, timeout=0):
+        def show(title, message, icon=dirs.res + "/instantshare.ico", timeout=0):
             from subprocess import Popen, PIPE
             import shlex
             cmd = shlex.split(
@@ -23,7 +24,7 @@ class Toast(Platform):
         self.show = show
 
     def init_windows(self):
-        def s(title, message, icon=None, timeout=0):
+        def s(title, message, icon=dirs.res + "/instantshare.ico", timeout=0):
             from win10toast import ToastNotifier
             toaster = ToastNotifier()
             toaster.show_toast(title, message, icon, timeout)
